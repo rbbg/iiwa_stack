92fea55fa5e2327739cf0c0a47a3762601535672
Ruben <Rubenburger@gmail.com>
2016-12-06 18:26:28 +0100

    Removed unnecesary import.

--- iiwa_ros_java/src/de/tum/in/camp/kuka/ros/iiwaConfiguration.java
+++ iiwa_ros_java/src/de/tum/in/camp/kuka/ros/iiwaConfiguration.java
@@ -39,7 +39,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
-import java.lang.RuntimeException
 
 import org.ros.exception.ParameterNotFoundException;
 import org.ros.namespace.GraphName;
